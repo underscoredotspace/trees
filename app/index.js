652fa8a9eff99df6ee466b965d731b4f98cf0739
Colin Tindle <ampersand@underscore.space>
2018-02-26 15:45:25 +0000

    Added sky + tree-snow Improved tree number and positioning

--- app/index.js
+++ app/index.js
@@ -3,11 +3,13 @@ const rnd = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 const treeTemplate = document.querySelector('#tree')
 const trees = document.querySelector("#trees")
 
-for (ndx = 1; ndx < (window.innerWidth/80); ndx++) {
+for (ndx = 1; ndx < (window.innerWidth/90); ndx++) {
   const tree = document.importNode(treeTemplate.content, true)
   const treeElement = tree.querySelector('.tree')
-  treeElement.style.marginBottom = `${rnd(5,30)}vh`
-  treeElement.style.transform = `translateX(${rnd(-30,30)}%)`
+  const margin = rnd(0,30)
+  treeElement.style.marginBottom = `${margin}vh`
+  treeElement.style.transform = `translateX(${rnd(-20,20)}%) scale(${1.5-(margin/30)})`
+  treeElement.style.zIndex = 30-margin
 
   trees.appendChild(tree)
 }
